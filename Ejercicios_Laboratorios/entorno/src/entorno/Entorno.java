e1f2f1e8fe034b563a390fbad67e52e4f807eaa1
SquintPhoenix85 <108373281+SquintPhoenix85@users.noreply.github.com>
2025-10-01 13:59:55 -0500

    Ejercicios trabajados en lab - 30 Septiembre

---
+++ Ejercicios_Laboratorios/entorno/src/entorno/Entorno.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package entorno;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author orarroyo
+ */
+
+public class Entorno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        validacion val = new validacion();
+        val.setBounds(80, 60, 600, 600);
+        val.setTitle("MiPrimerProyecto");
+        val.setVisible(true);
+    }
+    
+}
