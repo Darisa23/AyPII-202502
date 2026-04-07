dfe5ea3bfb1356b1054e4be4ed3edffbcb1079d3
Darisa23 <152227965+Darisa23@users.noreply.github.com>
2026-04-07 12:00:16 -0500

    Calculadora

---
+++ Ejercicios_Laboratorios/CalculadoraThaly/src/calculadorathaly/CalculadoraThaly.java
@@ -0,0 +1,16 @@
+package calculadorathaly;
+
+//@author staliana
+public class CalculadoraThaly {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        operaciones oper= new operaciones();
+        oper.setBounds(600, 200, 450, 450);
+        oper.setVisible(true);
+        oper.setTitle("Calculadora básica");
+    }
+    
+}
